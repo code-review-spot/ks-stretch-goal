ad7070a7ad984f1a92845ea2a070a38f35d02859
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-05-06 22:07:52 -0400

    cleaned up a bit and aiming at a non iframe embed code

--- embed.js
+++ embed.js
@@ -1,4 +1,21 @@
-alert(goal);
+var html = '<div id="sg-progress"><div id="sg-progress-bar"></div></div><div id="sg-meta"><div id="sg-percent"><div class="sg-meta-value"></div><div class="sg-meta-msg">funded</div></div><div id="sg-goal"><div class="sg-meta-value"></div><div class="sg-meta-msg">stretch goal</div></div></div>';
 
-var pledged= $("#pledged").data('pledged');
-alert(pledged);
+$("#sg-display").html(html);
+
+$("#sg-goal .sg-meta-value").html(goal);
+
+var oPledged = 0;
+
+var update = function(){
+	var pledged= $("#pledged").data('pledged');
+	if(pledged>=oPledged){
+		var percent = Math.round((100/goal)*pledged);
+	  $("#sg-percent .sg-meta-value").html(percent);
+	  $("#sg-progress-bar").css("width", percent+"%");
+	  oPledged = pledged;
+	}
+}
+
+update();
+
+setInterval(update, 1000);
