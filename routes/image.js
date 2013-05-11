ec4d979cd47ec5e4df766be4aa04745c39b3b166
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-05-11 19:19:44 -0400

    adding in webshot image create to see what happens on heroku

--- routes/image.js
+++ routes/image.js
@@ -5,12 +5,12 @@ var drawImage = function(res, imageName){
 }
 
 var createImage = function(model, callback){
-  console.log("HERE");
-  callback("ERR");
+  webshot('http://ks-stretch-goal.herokuapp.com/embed/'+model.id, './db/images/'+model.id+'.png', function(err) {
+    callback(err); 
+  });
 }
 
 module.exports.get = function(req, res){
-  console.log("START");
   var image = function(err, model){
     if(err){
       //draw error image
@@ -19,8 +19,6 @@ module.exports.get = function(req, res){
     else{
       var update_time = model.update_time;
       model.update(function(err, new_model){
-        console.log("NEW MODEL", new_model.update_time);
-        console.log("OLD MODEL", update_time);
         if(new_model.update_time!=update_time){
           //create new image
           createImage(new_model, function(err){
